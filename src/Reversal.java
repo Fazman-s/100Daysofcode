a3db8302afb72d358fcbf151a2f4e776220c827a
Fazman <fazmansam@gmail.com>
2023-05-11 01:33:39 +0530

    Reverse of an array

---
+++ src/Reversal.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+//Reverse of an array
+public class Reversal {
+    public static void main(String[] args) {
+        int[] arr={3,30,69,7,10,90};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(end>start){
+            //swap
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+    }
+
+    static void swap(int[] arr,int index1,int index2){
+        int temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+
+    }
+}
