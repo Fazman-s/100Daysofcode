b7b9068c73cfb27e9aeb2a8d3f7a16d81464ba94
Fazman-s <fazmansam@gmail.com>
2023-05-12 22:11:58 +0530

    Number of setbits

--- src/SetBits.java
+++ src/SetBits.java
@@ -2,7 +2,7 @@
 
 public class SetBits {
     public static void main(String[] args) {
-        int n=10;
+        int n=7;
         System.out.println(Integer.toBinaryString(n));
 
         System.out.println("The number of set bits are = "+setBits(n));
@@ -10,7 +10,7 @@ public class SetBits {
 
     private static int setBits(int n) {
         int count=0;
-        while(n>0){
+        while(n!=0){
             n=n & (n-1);   //n=8,n-1=7 i.e since n>0 -> count=1,(1000) & (111) = 0; now n=0--> out of loop
             count++;
 
