132bc068828a0a5836f894b2dbdb4c1af64c9ce6
Fazman-s <fazmansam@gmail.com>
2023-05-12 22:12:57 +0530

    Number of setbits

--- src/SetBits.java
+++ src/SetBits.java
@@ -15,6 +15,6 @@ public class SetBits {
             count++;
 
         }
-        return count;   //will return count=1 for the above example
+        return count;   //will return count=1 for the above example.
     }
 }
