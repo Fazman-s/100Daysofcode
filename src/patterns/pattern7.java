cb75b264e8842d1ce4c24b8665cbe48b4280b825
Fazman-s <fazmansam@gmail.com>
2023-06-03 22:49:12 +0530

    pattern7

---
+++ src/patterns/pattern7.java
@@ -0,0 +1,26 @@
+package patterns;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n) {
+        for(int row=1;row<= n;row++){
+            for(int spaces=0;spaces<n-row;spaces++){
+                System.out.print("  ");
+            }
+
+            for(int col=row; col>=1;col--){
+                System.out.print(col+" ");
+            }
+            for(int col=2;col<=row;col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
