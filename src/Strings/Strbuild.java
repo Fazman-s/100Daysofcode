15d482bc1b5d671506bc412a7129136b3f5fa292
Fazman-s <fazmansam@gmail.com>
2023-06-26 23:21:39 +0530

    StringBuilder functions

---
+++ src/Strings/Strbuild.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class Strbuild {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('a'+i);
+            builder.append(ch + " ");
+        }
+        System.out.println(builder);
+
+        builder.reverse();
+        System.out.println(builder);
+
+        builder.deleteCharAt(0);
+        System.out.println(builder);
+
+        builder.insert(0,"a ");
+        System.out.println(builder);
+    }
+}
