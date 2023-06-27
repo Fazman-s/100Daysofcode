e5718b6f8def0ef693a7374c66e14339f2014b74
Fazman-s <fazmansam@gmail.com>
2023-06-27 21:28:34 +0530

    Valid Paranthesis

---
+++ src/Strings/ValidParenthesis.java
@@ -0,0 +1,31 @@
+package Strings;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s=in.nextLine();
+        System.out.println(isValid(s));
+    }
+    public static boolean isValid(String s){
+        if(s.length()%2 !=0) return false;
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '{' || c == '[' || c=='('){
+                stack.push(c);
+            }
+            else if(c =='}' && !stack.isEmpty() && stack.peek() == '{'){
+                stack.pop();
+            } else if(c ==']' && !stack.isEmpty() && stack.peek() == '['){
+                stack.pop();
+            } else if(c ==')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
