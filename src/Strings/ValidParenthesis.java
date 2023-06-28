a8210f21fe0307ac20de72a85556e26d0125ec54
Fazman-s <fazmansam@gmail.com>
2023-06-28 18:36:43 +0530

    Valid Paranthesis

--- src/Strings/ValidParenthesis.java
+++ src/Strings/ValidParenthesis.java
@@ -10,22 +10,43 @@ public class ValidParenthesis {
         System.out.println(isValid(s));
     }
     public static boolean isValid(String s){
-        if(s.length()%2 !=0) return false;
         Stack<Character> stack = new Stack<>();
 
         for(char c : s.toCharArray()){
-            if(c == '{' || c == '[' || c=='('){
+            if(c == '{'|| c== '[' || c=='('){
                 stack.push(c);
+            }else if(c =='}' && !stack.isEmpty() && stack.peek()=='{'){
+                stack.pop();
+            }else if(c == ']' && !stack.isEmpty() && stack.peek() =='['){
+                stack.pop();
+            }else if(c == ')' && !stack.isEmpty() && stack.peek()=='('){
+                stack.pop();
+
+            }else {
+                return false;
             }
-            else if(c =='}' && !stack.isEmpty() && stack.peek() == '{'){
-                stack.pop();
-            } else if(c ==']' && !stack.isEmpty() && stack.peek() == '['){
-                stack.pop();
-            } else if(c ==')' && !stack.isEmpty() && stack.peek() == '('){
-                stack.pop();
-            }
+
         }
         return stack.isEmpty();
 
+
     }
 }
+
+
+//       if(s.length()%2 !=0) return false;
+//               Stack<Character> stack = new Stack<>();
+//
+//        for(char c : s.toCharArray()){
+//        if(c == '{' || c == '[' || c=='('){
+//        stack.push(c);
+//        }
+//        else if(c =='}' && !stack.isEmpty() && stack.peek() == '{'){
+//        stack.pop();
+//        } else if(c ==']' && !stack.isEmpty() && stack.peek() == '['){
+//        stack.pop();
+//        } else if(c ==')' && !stack.isEmpty() && stack.peek() == '('){
+//        stack.pop();
+//        }
+//        }
+//        return stack.isEmpty();
