e3eb0062fd34688b04020a40e61854f7668e3805
Fazman-s <fazmansam@gmail.com>
2023-08-27 23:16:04 +0530

    Saddle Point Matrix

--- src/randomprob/SaddlePointMatrix.java
+++ src/randomprob/SaddlePointMatrix.java
@@ -24,13 +24,11 @@ public class SaddlePointMatrix {
             // the maximum element of column or not
             int k;
             for (k = 0; k < n; k++)
-
                 // Note that col_ind is fixed
                 if (min_row < mat[k][col_ind])
                     break;
 
-            // If saddle point is present in this row then
-            // print it
+            // If saddle point is present in this row then print it
             if (k == n)
             {
                 System.out.println("Value of Saddle Point " + min_row);
