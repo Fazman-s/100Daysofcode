e8264ee11003f1128c19314b80b90720da8b16c0
Fazman-s <fazmansam@gmail.com>
2023-09-14 18:32:51 +0530

    pattern9

---
+++ src/patterns/pattern9.java
@@ -0,0 +1,27 @@
+package patterns;
+
+public class pattern9 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n) {
+        for(int i=0;i<n;i++){
+            //space
+            for(int j=0; j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0; j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
