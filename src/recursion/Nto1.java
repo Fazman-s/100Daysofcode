3488f6837d523f789dfded220184631b69c2d7ca
Fazman-s <fazmansam@gmail.com>
2023-10-02 20:31:55 +0530

    print n to 1 using recursion

---
+++ src/recursion/Nto1.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class Nto1 {
+    public static void main(String[] args) {
+        fun(5);
+
+    }
+
+    static void fun(int n){
+        if(n==0){
+            return;
+        }
+
+        System.out.println(n);
+        fun(n-1);
+    }
+}
