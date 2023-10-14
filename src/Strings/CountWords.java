c0d8aef8504364b5385ebdea47008d57f1fc9719
Fazman-s <fazmansam@gmail.com>
2023-10-14 22:16:16 +0530

    pattern

---
+++ src/Strings/CountWords.java
@@ -0,0 +1,19 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String str=in.nextLine();
+        System.out.println(countwords(str));
+    }
+
+    public static int countwords(String str){
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+        String[] words = str.split(" +");
+        return words.length;
+    }
+}
--- src/patterns/pattern17.java
+++ src/patterns/pattern17.java
@@ -1,16 +1,6 @@
 package patterns;
 
-/*
-    *
-   ***
-  *****
- *******
-*********
-
-*/
-
-
-public class pattern9 {
+public class pattern17 {
     public static void main(String[] args) {
 
         pattern(5);
@@ -22,9 +12,10 @@ public class pattern9 {
             for(int j=0; j<n-i-1;j++){
                 System.out.print(" ");
             }
-            //star
+            //numbers
             for(int j=0;j<2*i+1;j++){
-                System.out.print("*");
+                System.out.print(i);
+
             }
             //space
             for(int j=0; j<n-i-1;j++){
